55365376771e8e76d5ff562a4b9f95ec30db6d3e
두주 <dlgkswn885@korea.ac.kr>
2020-08-01 15:03:36 +0900

    hanju

--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 import { withRouter } from "react-router-dom";
 import { loginUser } from "../../../_actions/user_actions";
-import { Formik } from 'formik';
-import * as Yup from 'yup';
-import { Form, Icon, Input, Button, Checkbox, Typography } from 'antd';
+import { Formik } from "formik";
+import * as Yup from "yup";
+import { Form, Icon, Input, Button, Checkbox, Typography } from "antd";
 import { useDispatch } from "react-redux";
 
 const { Title } = Typography;
@@ -12,61 +12,64 @@ function LoginPage(props) {
   const dispatch = useDispatch();
   const rememberMeChecked = localStorage.getItem("rememberMe") ? true : false;
 
-  const [formErrorMessage, setFormErrorMessage] = useState('')
-  const [rememberMe, setRememberMe] = useState(rememberMeChecked)
+  const [formErrorMessage, setFormErrorMessage] = useState("");
+  const [rememberMe, setRememberMe] = useState(rememberMeChecked);
 
   const handleRememberMe = () => {
-    setRememberMe(!rememberMe)
+    setRememberMe(!rememberMe);
   };
 
-  const initialEmail = localStorage.getItem("rememberMe") ? localStorage.getItem("rememberMe") : '';
+  const initialEmail = localStorage.getItem("rememberMe")
+    ? localStorage.getItem("rememberMe")
+    : "";
 
   return (
     <Formik
       initialValues={{
         email: initialEmail,
-        password: '',
+        password: "",
       }}
       validationSchema={Yup.object().shape({
         email: Yup.string()
-          .email('Email is invalid')
-          .required('Email is required'),
+          .email("이메일 형식에 맞지 않습니다")
+          .required("이메일을 꼭 입력해주세요"),
         password: Yup.string()
-          .min(6, 'Password must be at least 6 characters')
-          .required('Password is required'),
+          .min(6, "비밀번호는 최소 6자리 이상입니다.")
+          .required("비밀번호를 꼭 입력해주세요!"),
       })}
       onSubmit={(values, { setSubmitting }) => {
         setTimeout(() => {
           let dataToSubmit = {
             email: values.email,
-            password: values.password
+            password: values.password,
           };
 
           dispatch(loginUser(dataToSubmit))
-            .then(response => {
+            .then((response) => {
               if (response.payload.loginSuccess) {
-                window.localStorage.setItem('userId', response.payload.userId);
+                console.log(response.payload);
+                window.localStorage.setItem("userId", response.payload.userId);
                 if (rememberMe === true) {
-                  window.localStorage.setItem('rememberMe', values.id);
+                  window.localStorage.setItem("rememberMe", values.email);
                 } else {
-                  localStorage.removeItem('rememberMe');
+                  localStorage.removeItem("rememberMe");
                 }
                 props.history.push("/");
               } else {
-                setFormErrorMessage('Check out your Account or Password again')
+                setFormErrorMessage("이메일이나 비밀번호를 확인해주세요");
               }
             })
-            .catch(err => {
-              setFormErrorMessage('Check out your Account or Password again')
+            .catch((err) => {
+              setFormErrorMessage("이메일이나 비밀번호를 확인해주세요");
               setTimeout(() => {
-                setFormErrorMessage("")
+                setFormErrorMessage("");
               }, 3000);
             });
           setSubmitting(false);
         }, 500);
       }}
     >
-      {props => {
+      {(props) => {
         const {
           values,
           touched,
@@ -80,21 +83,24 @@ function LoginPage(props) {
         } = props;
         return (
           <div className="app">
-
-            <Title level={2}>Log In</Title>
-            <form onSubmit={handleSubmit} style={{ width: '350px' }}>
-
+            <Title level={2}>Hack Your Life</Title>
+            {/* <Title level={}>Log In</Title> */}
+            <form onSubmit={handleSubmit} style={{ width: "350px" }}>
               <Form.Item required>
                 <Input
                   id="email"
-                  prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                  placeholder="Enter your email"
+                  prefix={
+                    <Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />
+                  }
+                  placeholder="이메일을 입력해주세요"
                   type="email"
                   value={values.email}
                   onChange={handleChange}
                   onBlur={handleBlur}
                   className={
-                    errors.email && touched.email ? 'text-input error' : 'text-input'
+                    errors.email && touched.email
+                      ? "text-input error"
+                      : "text-input"
                   }
                 />
                 {errors.email && touched.email && (
@@ -105,14 +111,18 @@ function LoginPage(props) {
               <Form.Item required>
                 <Input
                   id="password"
-                  prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                  placeholder="Enter your password"
+                  prefix={
+                    <Icon type="lock" style={{ color: "rgba(0,0,0,.25)" }} />
+                  }
+                  placeholder="비밀번호를 입력해주세요"
                   type="password"
                   value={values.password}
                   onChange={handleChange}
                   onBlur={handleBlur}
                   className={
-                    errors.password && touched.password ? 'text-input error' : 'text-input'
+                    errors.password && touched.password
+                      ? "text-input error"
+                      : "text-input"
                   }
                 />
                 {errors.password && touched.password && (
@@ -121,20 +131,49 @@ function LoginPage(props) {
               </Form.Item>
 
               {formErrorMessage && (
-                <label ><p style={{ color: '#ff0000bf', fontSize: '0.7rem', border: '1px solid', padding: '1rem', borderRadius: '10px' }}>{formErrorMessage}</p></label>
+                <label>
+                  <p
+                    style={{
+                      color: "#ff0000bf",
+                      fontSize: "0.7rem",
+                      border: "1px solid",
+                      padding: "1rem",
+                      borderRadius: "10px",
+                    }}
+                  >
+                    {formErrorMessage}
+                  </p>
+                </label>
               )}
 
               <Form.Item>
-                <Checkbox id="rememberMe" onChange={handleRememberMe} checked={rememberMe} >Remember me</Checkbox>
-                <a className="login-form-forgot" href="/reset_user" style={{ float: 'right' }}>
-                  forgot password
-                  </a>
+                <Checkbox
+                  id="rememberMe"
+                  onChange={handleRememberMe}
+                  checked={rememberMe}
+                >
+                  이메일 저장하기
+                </Checkbox>
+                <a
+                  className="login-form-forgot"
+                  href="/reset_user"
+                  style={{ float: "right" }}
+                >
+                  비밀번호 찾기
+                </a>
                 <div>
-                  <Button type="primary" htmlType="submit" className="login-form-button" style={{ minWidth: '100%' }} disabled={isSubmitting} onSubmit={handleSubmit}>
-                    Log in
-                </Button>
+                  <Button
+                    type="primary"
+                    htmlType="submit"
+                    className="login-form-button"
+                    style={{ minWidth: "100%" }}
+                    disabled={isSubmitting}
+                    onSubmit={handleSubmit}
+                  >
+                    로그인
+                  </Button>
                 </div>
-                Or <a href="/register">register now!</a>
+                <a href="/register">아직 회원이 아니라면?!</a>
               </Form.Item>
             </form>
           </div>
@@ -142,8 +181,6 @@ function LoginPage(props) {
       }}
     </Formik>
   );
-};
+}
 
 export default withRouter(LoginPage);
-
-
